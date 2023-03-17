50943e364ee5aca709861e0f592f35634ebcc955
bharath1617 <106333419+bharath1617@users.noreply.github.com>
2023-03-17 19:54:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 //your code here
+const bands = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
 
+const strip = (bandName) => bandName.replace(/^(a |the |an )/i, "").trim();
+// use space between a, the or and to make sure that you catch whole word or not miss any word in the actual band name!
+
+const sortedBands = bands.sort((a, b) => strip(a) > strip(b) ? 1 : -1);
+
+document.querySelector("#bands").innerHTML = sortedBands.map(band =>`<li>${band}</li>`).join(""); // join will join the array into a big string rather than a bunch of strings with a comma in between!
